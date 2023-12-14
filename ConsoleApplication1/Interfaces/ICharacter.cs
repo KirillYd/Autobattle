e12b17f2e0d8367864e1462c14de5f55dba77f7c
KirillYd <k.yudanov2003@mail.ru>
2023-12-14 13:42:40 +0500

    first commit

---
+++ ConsoleApplication1/Interfaces/ICharacter.cs
@@ -0,0 +1,14 @@
+namespace ConsoleApplication1.Interfaces
+{
+    public interface ICharacter
+    {
+        double damage { get; set; }
+        double head { get; set; }
+        double chest { get; set; }
+        double stomach { get; set; }
+        double leftArm { get; set; }
+        double leftLeg { get; set; }
+        double rightArm { get; set; }
+        double rightLeg { get; set; }
+    }
+}
