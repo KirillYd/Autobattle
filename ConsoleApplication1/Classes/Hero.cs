78aac77bc138de3842ef6cc0a40457fd948ece7b
Andrew <zotovandrey36@gmail.com>
2023-12-20 18:46:27 +0500

    commit 2

--- ConsoleApplication1/Classes/Hero.cs
+++ ConsoleApplication1/Classes/Hero.cs
@@ -2,10 +2,18 @@ using ConsoleApplication1.Interfaces;
 
 namespace ConsoleApplication1
 {
-    public class Hero
+    public class Hero: ICharacter
     {
         public readonly MeleeWeapon meleeWeapon;
         public readonly RangedWeapon rangedWeapon;
         private ICharacter heroStats;
+        public double damage { get; set; }
+        public double head { get; set; }
+        public double chest { get; set; }
+        public double stomach { get; set; }
+        public double leftArm { get; set; }
+        public double leftLeg { get; set; }
+        public double rightArm { get; set; }
+        public double rightLeg { get; set; }
     }
 }
